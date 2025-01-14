bf17d2179382d35f3bd02b69af004f4b970526fe
Mahmud Rashid <srabon5704@gmail.com>
2025-01-14 23:28:10 +0600

    finished home page sections

--- src/pages/home/SuccessStories.jsx
+++ src/pages/home/SuccessStories.jsx
@@ -1,57 +1,72 @@
-import SectionTitle from "../../components/SectionTitle"
+import SectionTitle from '../../components/SectionTitle'
+import ReactStars from 'react-rating-stars-component';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const SuccessStories = () => {
+    const [stories, setStories] = useState([]);
+
+    useEffect(() => {
+        axios.get("/success.json")
+            .then(res => setStories(res.data))
+    }, []);
+
+    console.log(stories)
+
     return (
         <section className="w-11/12 md:container xl:w-9/12 mx-auto mb-14 lg:mb-24">
             <SectionTitle
                 heading="Real Stories, Real Success"
                 subHeading="Thousands of students have turned their dreams into reality with the help of our platform. Read their inspiring stories!"
             />
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-                <div className="lg:flex-col bg-white shadow-lg rounded-lg overflow-hidden flex items-center p-4">
-                    <img src="https://randomuser.me/api/portraits/women/1.jpg" alt="Emily Carter" className="w-24 h-24 rounded-full object-cover mr-4 self-start" />
-                    <div>
-                        <h3 className="font-semibold text-lg text-brand-text">Emily Carter</h3>
-                        <p className="text-sm text-gray-500">USA to Canada</p>
-                        <p className="text-gray-600 mt-2 text-base sm:text-sm md:text-base">"Thanks to this platform, I received a full scholarship for my Master's in Computer Science at the University of Toronto. The process was easy and stress-free!"</p>
-                    </div>
-                </div>
+            <div>
+                <Swiper
+                    slidesPerView={1}
+                    centeredSlides={true}
+                    spaceBetween={0}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    breakpoints={{
+                        640: { slidesPerView: 2, spaceBetween: 20, centeredSlides: false },
+                        768: { slidesPerView: 3, spaceBetween: 30, centeredSlides: false },
+                        1024: { slidesPerView: 4, spaceBetween: 40, centeredSlides: false },
+                    }}
+                    className="mySwiper"
+                >
+                    {stories?.map((story, index) => (
+                        <SwiperSlide key={index} className="pb-14 h-full hover:cursor-grab">
+                            <div className="max-w-xs bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 duration-300 my-4 h-full flex flex-col py-4">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        src={story.image}
+                                        alt={story.name + ' image'}
+                                        className="w-24 h-24 rounded-full object-cover mx-auto border-4 border-white shadow-lg"
+                                    />
+                                </div>
+                                <div className="px-6 py-2 text-center flex-1 flex flex-col justify-between">
+                                    <h3 className="font-semibold text-xl text-brand-text">{story.name}</h3>
+                                    <p className="text-sm text-gray-500">{story.location}</p>
+                                    <div className="flex justify-center">
+                                        <ReactStars count={5} value={story.rating} size={24} edit={false} activeColor="#ffd700" />
+                                    </div>
+                                    <p className="text-gray-600 text-sm md:text-base mt-2">
+                                        {story.testimonial}
+                                    </p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
 
-                <div className="lg:col-span-2 bg-white shadow-lg rounded-lg overflow-hidden flex items-center p-4 lg:flex-col">
-                    <img src="https://randomuser.me/api/portraits/men/2.jpg" alt="Omar Ibrahim" className="w-24 h-24 rounded-full object-cover mr-4 self-start" />
-                    <div>
-                        <h3 className="font-semibold text-lg text-brand-text">Omar Ibrahim</h3>
-                        <p className="text-sm text-gray-500">Egypt to Germany</p>
-                        <p className="text-gray-600 mt-2 text-base sm:text-sm md:text-base">"I was awarded a scholarship to study Engineering in Germany. The platform helped me find the perfect scholarship and guided me through every step!"</p>
-                    </div>
-                </div>
-
-                <div className="sm:col-span-2 lg:col-span-1 lg:row-span-2 bg-white shadow-lg rounded-lg overflow-hidden flex lg:flex-col items-center p-4">
-                    <img src="https://randomuser.me/api/portraits/women/3.jpg" alt="Isabella Martinez" className="w-24 h-24 rounded-full object-cover mr-4" />
-                    <div>
-                        <h3 className="font-semibold text-lg text-brand-text">Isabella Martinez</h3>
-                        <p className="text-sm text-gray-500">Mexico to the UK</p>
-                        <p className="text-gray-600 mt-2 text-base sm:text-sm md:text-base">"I never thought studying abroad would be possible for me, but this platform helped me secure a scholarship for my Master's in London. I'm forever grateful!"</p>
-                    </div>
-                </div>
-
-                <div className="lg:col-span-2 bg-white shadow-lg rounded-lg overflow-hidden flex items-center p-4 lg:flex-col ">
-                    <img src="https://randomuser.me/api/portraits/men/4.jpg" alt="David Williams" className="w-24 h-24 rounded-full object-cover mr-4 self-start" />
-                    <div>
-                        <h3 className="font-semibold text-lg text-brand-text">David Williams</h3>
-                        <p className="text-sm text-gray-500">Australia to France</p>
-                        <p className="text-gray-600 mt-2 text-base sm:text-sm md:text-base">"I found the ideal scholarship program in France with this platform. I couldn’t have asked for an easier process or more support along the way!"</p>
-                    </div>
-                </div>
-
-                <div className="bg-white shadow-lg rounded-lg overflow-hidden flex lg:flex-col items-center p-4">
-                    <img src="https://randomuser.me/api/portraits/women/5.jpg" alt="Chiamaka Okafor" className="w-24 h-24 rounded-full object-cover mr-4 self-start" />
-                    <div>
-                        <h3 className="font-semibold text-lg text-brand-text">Chiamaka Okafor</h3>
-                        <p className="text-sm text-gray-500">Nigeria to Canada</p>
-                        <p className="text-gray-600 mt-2 text-base sm:text-sm md:text-base">"I always dreamed of studying in Canada, and this platform made it possible. I received a full scholarship for my undergraduate studies at the University of Alberta!"</p>
-                    </div>
-                </div>
+                    ))}
+                </Swiper>
             </div>
         </section>
     )
